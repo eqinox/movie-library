4db4f6675f4bf34779c04d90796d501eede548f8
eqinox <86393056685015v>
2021-07-16 16:28:42 +0300

    remove npm6 and some logs

--- server/server/auth/authentication.js
+++ server/server/auth/authentication.js
@@ -8,8 +8,7 @@ module.exports = {
     console.log(token);
     token.replace(/Bearer\s+(.+)/i, (a, b) => {
       let token = b;
-      console.log("does we log in");
-      console.log(token);
+      
       if (!token) return res.status(200).json({ success: false, message: 'No token provided'});
       
       jwt.verify(token, settings.development.secret, (err, decoded) => {
